4552e69da6938e0811c1e7cc1518276565146b3b
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2026-01-24 16:35:46 -0300

    :recycle: refac: separated book form from creation page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,87 @@
 "use client";
 
+import { useState } from "react";
 import Select from "@/lib/components/select";
+import { createOption } from "@/lib/components/select/lib";
+import search from "@/lib/util/search";
+import CreatableSelect from "react-select/creatable";
 
-const options = [
-  { label: "Risotto", value: "risotto" },
-  { label: "Sandwich", value: "sandwich" },
-  { label: "Rice", value: "rice" },
-  { label: "Beans", value: "beans" },
-  { label: "Donut", value: "donut" },
-];
+import type { DefaultGroup, DefaultOption } from "@/lib/components/select/types";
+import { PropsValue } from "react-select";
+
+const defaultOptions = [
+  createOption("Salame"),
+  createOption("Sandwich"),
+  createOption("Mushroom Stew"),
+]
 
 export default function Home() {
+  const [creatable, setCreatable] = useState(true);
+  const [value, setValue] = useState<PropsValue<DefaultOption>>(createOption("Waffle"));
+  const [options, setOptions] = useState([
+    createOption("Risotto"),
+    createOption("Sandwich"),
+    createOption("Rice"),
+    createOption("Beans"),
+    createOption("Donut"),
+  ]);
+
   return (
-    <Select
-      options={options}
-      formatCreateLabel={() => <></>}
-      creatable={false}
-      onMenuClose={() => {}}
-    />
+    <>
+      <Select<DefaultOption, false, DefaultGroup>
+        loadOptions={
+          (inputValue) => new Promise(
+            (resolve) => setTimeout(
+              () => resolve(options),
+              2000
+            )
+          )
+        }
+        placeholder="Foods"
+        autoload={true}
+        // defaultOptions={defaultOptions}
+        // allowCreateWhileLoading={true}
+        // value={createOption("Waffle")}
+        styles={{
+          container: (base) => ({
+            ...base,
+            color: "black",
+          }),
+        }}
+        isClearable
+        // defaultValue={createOption("Peanuts")}
+        loadDefaultValue={
+          () => new Promise(
+            (resolve) => setTimeout(
+              () => resolve(createOption("Butter")),
+              1000
+            )
+          )
+        }
+      />
+      <div>
+        <label>Creatable: </label>
+        <input
+          type="checkbox"
+          checked={creatable}
+          onChange={e => setCreatable(e.target.checked)}
+        />{" "}
+        <span>{creatable ? "Yes" : "No" }</span>
+      </div>
+      <CreatableSelect
+        options={options}
+        styles={{
+          container: (base) => ({
+            ...base,
+            color: "black",
+          }),
+        }}
+        value={value}
+        onChange={(newValue, actionMeta) => {
+          setValue(newValue)
+          console.log(newValue, actionMeta)
+        }}
+      />
+    </>
   );
 }
