9a98f01d974690686f5e45d0c9ecae0cf7fb16a4
Daniel O. Sousa <97067805+nadjiel@users.noreply.github.com>
2026-01-25 15:56:05 -0300

    :recycle: refac: manually tested integration

--- app/books/new/page.tsx
+++ app/books/new/page.tsx
@@ -1,5 +1,17 @@
+"use client";
+
 import BookForm from "@/book/form";
+import type { Book } from "@/api/books/types";
+
+const data: Book = {
+  id: "3",
+  name: "Echoes of Silence",
+  description: "A gripping story of survival and hope.",
+  publicationDate: "2015-11-10",
+  authorId: "3",
+  rating: 4.5
+}
 
 export default function CreateBook() {
-  return <BookForm />
+  return <BookForm data={data} onValid={console.log} onInvalid={console.error} />
 }
